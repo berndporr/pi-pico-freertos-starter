a768c51635e0d3461e4c7e6a9c7533b20261a4b8
Bernd Porr <mail@berndporr.me.uk>
2025-11-02 09:31:57 +0000

    Fixed the delay

--- main.c
+++ main.c
@@ -7,9 +7,7 @@
 
 #define MY_TASK_PRIORITY  2
 
-#ifndef LED_DELAY_MS
 #define LED_DELAY_MS 250
-#endif
 
 // gpio pin number
 const int n = 15;
@@ -29,57 +27,26 @@ static void my_task(void *data) {
     (void)data; // unused parameter
     bool b = false;
     
-    printf("user task started\n");
-
     for (;;) {
-        // Do something interesting here
-        printf("My task is running!\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        // Do something interesting and not just one task with a delay in it.
+	// We supposed to be doing multitasking!
+        vTaskDelay(pdMS_TO_TICKS(LED_DELAY_MS));
 	pico_set_led(b);
 	b = !b;
     }
-    // Do not let a task procedure return
     vTaskDelete(NULL);
 }
 
 int main() {
-    stdio_init_all();
-
+    // init the GPIO for the LED
     pico_led_init();
 
+    // create an freeRTOS task
     xTaskCreate(my_task, "application_task", configMINIMAL_STACK_SIZE, NULL, MY_TASK_PRIORITY, NULL);
 
+    // this starts effectively freeRTOS and should never return.
     vTaskStartScheduler();
-    // we should never return from FreeRTOS
+
+    // we should have never returned from FreeRTOS!
     panic_unsupported();
 }
-
-/*
-* These functions are requried for FreeRTOS to work in static memory mode.
-*/
-
-#if configSUPPORT_STATIC_ALLOCATION
-static StaticTask_t idle_task_tcb;
-static StackType_t idle_task_stack[mainIDLE_TASK_STACK_DEPTH];
-void vApplicationGetIdleTaskMemory(
-    StaticTask_t **ppxIdleTaskTCBBuffer,
-    StackType_t **ppxIdleTaskStackBuffer,
-    uint32_t *pulIdleTaskStackSize
-) {
-    *ppxIdleTaskTCBBuffer = &idle_task_tcb;
-    *ppxIdleTaskStackBuffer = idle_task_stack;
-    *pulIdleTaskStackSize = mainIDLE_TASK_STACK_DEPTH;
-}
-
-static StaticTask_t timer_task_tcb;
-static StackType_t timer_task_stack[configMINIMAL_STACK_SIZE];
-void vApplicationGetTimerTaskMemory(
-    StaticTask_t **ppxTimerTaskTCBBuffer,
-    StackType_t **ppxTimerTaskStackBuffer,
-    uint32_t *pulTimerTaskStackSize
-) {
-    *ppxTimerTaskTCBBuffer = &timer_task_tcb;
-    *ppxTimerTaskStackBuffer = timer_task_stack;
-    *pulTimerTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-#endif
